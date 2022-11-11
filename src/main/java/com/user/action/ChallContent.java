1c442347c2f883c77366aa06b7243ac524947da1
JadenFM <111298803+JadenFM@users.noreply.github.com>
2022-11-11 11:54:45 +0900

    Update ChallContent.java

--- src/main/java/com/user/action/ChallContent.java
+++ src/main/java/com/user/action/ChallContent.java
@@ -37,16 +37,21 @@ public class ChallContent implements Action {
 		UserDTO user_dto = join_dao.getMemInfo(createrNum);
 		request.setAttribute("userInfo", user_dto);
 		
-		// 참가 여부 정보 받아오기(챌린지 상세페이지 들어갔을 때 참가한 챌린지인지 체크)
-		int mem_num = (Integer) session.getAttribute("memberNum");
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(false);
+		int mem_num = 0;
+		if(session.getAttribute("memberNum")==null) {
+			// 로그인 안됐으면 로그인 창으로 
+			forward.setPath("member_login.do");
+		}else {
+			// 참가 여부 정보 받아오기(챌린지 상세페이지 들어갔을 때 참가한 챌린지인지 체크)
+			mem_num = (Integer) session.getAttribute("memberNum");
+			forward.setPath("user/member_challContent.jsp");
+		}
 		ChallProofDAO proof_dao = ChallProofDAO.getInstance();
 		int checkJoin = proof_dao.checkJoin(chall_num, mem_num);
 		request.setAttribute("checkJoin", checkJoin);	// 1-정보있음(버튼:'참가완료') / 2-정보없음(버튼:'참가하기')
 		System.out.println("checkJoin >>> "+checkJoin);
-		
-		ActionForward forward = new ActionForward();
-		forward.setRedirect(false);
-		forward.setPath("user/member_challContent.jsp");
 		
 		/*
 		 * Cookie ck_title = new Cookie("title", chall_dto.getChall_title()); Cookie
