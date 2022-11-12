ea7749edf005dbb7bccf46aea7a75efee9092c0a
dltprjs1 <107850856+dltprjs1@users.noreply.github.com>
2022-11-12 17:52:48 +0900

    Update AdminJoinAction.java

--- src/main/java/com/admin/action/AdminJoinAction.java
+++ src/main/java/com/admin/action/AdminJoinAction.java
@@ -18,9 +18,10 @@ public class AdminJoinAction implements Action {
 		String admin_id = request.getParameter("admin_id").trim();
 		String admin_pwd = request.getParameter("admin_pwd").trim();
 		String admin_name = request.getParameter("admin_name").trim();
-		int admin_age = Integer.parseInt(request.getParameter("admin_age").trim());
-		String admin_addr = request.getParameter("admin_addr").trim();
 		String admin_phone = request.getParameter("admin_phone").trim();
+		String admin_email_head = request.getParameter("admin_email_head").trim();
+		String admin_email_body = request.getParameter("admin_email_body").trim();
+		String admin_email = admin_email_head+admin_email_body;
 		
 		AdminDTO dto = new AdminDTO();
 		AdminDAO dao = AdminDAO.getInstance();
@@ -28,17 +29,16 @@ public class AdminJoinAction implements Action {
 		dto.setAdmin_id(admin_id);
 		dto.setAdmin_pwd(admin_pwd);
 		dto.setAdmin_name(admin_name);
-		dto.setAdmin_age(admin_age);
-		dto.setAdmin_addr(admin_addr);
 		dto.setAdmin_phone(admin_phone);
+		dto.setAdmin_email(admin_email);
 		
 		int res = dao.adminJoin(dto);
 		PrintWriter out = response.getWriter();
 		ActionForward forward = new ActionForward();
 		
 		if(res > 0 ) {
-			forward.setRedirect(false);
-			forward.setPath("/admin/admin_home.jsp");
+			forward.setRedirect(true);
+			forward.setPath("admin_login.do");
 		}else {
 			out.println("<script>");
 			out.println("alert('가입 실패!')");
